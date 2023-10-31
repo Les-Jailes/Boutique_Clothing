f71c74dae2280ab3ac37d2f6baa23023ee201291
victor-villca <villcavictorleon@gmail.com>
2023-10-31 00:11:51 -0400

    nav bar footer meny options creation and styling

---
+++ src/utils/navfooterItems.js
@@ -0,0 +1,26 @@
+export const navFooterItems = [
+    {
+      title:'Women',
+      url: '/women',
+    },
+    {
+        title:'Men',
+        url: '/men',
+    },
+    {
+        title:'Boy',
+        url: '/boy',
+    },
+    {
+        title:'Girl',
+        url: '/girl ',
+    },
+    {
+        title:'Summer',
+        url: '/summer',
+    },
+    {
+        title:'Offer',
+        url: '/offer',
+    },
+  ];
