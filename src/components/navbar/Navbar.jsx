80c7b6d8a2be9e04d75ddcbb2ca41bbfaf658639
victor-villca <villcavictorleon@gmail.com>
2023-10-30 21:09:07 -0400

    navbar component creation searcher and logo

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './navbar.module.css'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <div>
+        LOGO
+        <div>
+            <form action="" className='search-bar'>
+                <input type="text" placeholder='Search ...' className='search-input'/>
+                <button type='submit'>
+                    <Image
+                        src={"/searcher.png"}
+                        alt='search img'
+                        width={40}
+                        height={40}
+                     />
+                </button>
+            </form>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
