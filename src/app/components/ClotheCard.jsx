fa56c99af5bf5a30c43d7e3a406cc1c583f3aa73
denisgandel <denis.gandarillas.9355205@usalesiana.edu.bo>
2023-10-31 21:53:53 -0400

    Products page

--- src/app/components/ClotheCard.jsx
+++ src/app/components/ClotheCard.jsx
@@ -1,27 +1,58 @@
-import React from "react";
-import "@/app/css/ClotheCard.css";
-import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
+'use client'
+
+import "@/app/css/ClotheCard.css"
+import "@/app/css/General.css"
+import { AiOutlineHeart, AiFillHeart, AiOutlineShoppingCart } from "react-icons/ai"
+import { useState } from "react"
+import { ColorClothe } from "./ColorClothe"
 
 export const ClotheCard = ({ clothe }) => {
+
+  const [ isLiked, setIsLiked ] = useState(false)
+
+  const handleLike = () => {
+    setIsLiked(!isLiked)
+  }
+
   return (
     <div className="clothe-card-container">
-      <div className="card-image-section">
-        <img src={clothe.image} alt="Clothe image" className="clothe-image" draggable="false" />
+      <div className={ `card-image-section ${ clothe.category }` }>
+        <img
+          src={clothe.image}
+          alt="Clothe image"
+          className="clothe-image"
+          draggable="false"
+        />
       </div>
-      <div className="text-container">
-        <div className="clothe-information">
+      <div className="information-container">
+        <div className="section-card clothe-information">
           <h3 className="clothe-name">{clothe.name}</h3>
           <p className="clothe-price">{clothe.price} $</p>
         </div>
-        <div className="card-buttons">
-          <button className="options-card like-card">
-            <AiOutlineHeart />
+        <div className="section-card colors-container">
+          {
+            clothe.colors.map((color, index) => (
+              <ColorClothe key={ index } color={ color } />
+            ))
+          }
+        </div>
+        <div className="section-card card-buttons">
+          <button 
+            className="options-card like-card"
+            onClick={ () => handleLike() }
+          >
+            {
+              !isLiked ?
+              <AiOutlineHeart color="red" />
+              :
+              <AiFillHeart color="red" />
+            }
           </button>
           <button className="options-card shop-card">
-            <AiOutlineShoppingCart />
+            <AiOutlineShoppingCart color="white" />
           </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
--- src/app/pages/products/page.jsx
+++ src/app/pages/products/page.jsx
@@ -1,20 +1,119 @@
-import { ClotheCard } from "@/app/components/ClotheCard";
+'use client'
 
-const exampleProduct = {
-  "name": "Clothe name",
-  "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
-  "price": 650,
-  "category": "Women",
-  "colors": ['red', 'blue', 'green', 'black']
-}
+import { ClotheCard } from "@/app/components/ClotheCard"
+import '@/app/css/General.css'
+import '@/app/css/ProductsPage.css'
+import { useState } from "react"
+import { AiFillFilter } from "react-icons/ai";
+
+const examplesProduct = [
+  {
+    "name": "Producto 1",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 650,
+    "category": "women",
+    "colors": ['red']
+  },
+  {
+    "name": "Producto 2",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 750,
+    "category": "men",
+    "colors": ['blue', 'black']
+  },
+  {
+    "name": "Producto 3",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 450,
+    "category": "boy",
+    "colors": ['green', 'gray']
+  },
+  {
+    "name": "Producto 4",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 550,
+    "category": "girl",
+    "colors": ['pink', 'purple', 'yellow']
+  },
+  {
+    "name": "Producto 5",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 600,
+    "category": "women",
+    "colors": ['blue', 'black']
+  },
+  {
+    "name": "Producto 6",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 800,
+    "category": "men",
+    "colors": ['red']
+  },
+  {
+    "name": "Producto 7",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 500,
+    "category": "boy",
+    "colors": ['green', 'gray']
+  },
+  {
+    "name": "Producto 8",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 700,
+    "category": "girl",
+    "colors": ['pink']
+  },
+  {
+    "name": "Producto 9",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 750,
+    "category": "women",
+    "colors": ['blue']
+  },
+  {
+    "name": "Producto 10",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 550,
+    "category": "men",
+    "colors": ['red', 'black']
+  },
+  {
+    "name": "Producto 11",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 650,
+    "category": "boy",
+    "colors": ['green']
+  },
+  {
+    "name": "Producto 12",
+    "image": "https://i.postimg.cc/cJtVQVGq/image-removebg-preview-4.png",
+    "price": 700,
+    "category": "girl",
+    "colors": ['pink', 'yellow']
+  }
+]
 
 export default function page() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpenFilter = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div>
-      <div className="filter-container"></div>
+    <div className="products-page">
+      <button className={ ` button-filter-container ${ isOpen ? 'is-open' : '' } ` } onClick={ () => handleOpenFilter() }>
+        <AiFillFilter color="white" />
+      </button>
+      <div className={ ` filter-container ${ isOpen ? 'is-open' : '' } ` }></div>
       <div className="product-container">
-        <ClotheCard clothe={ exampleProduct } />
+        {
+          examplesProduct.map((exampleProduct, index) => (
+            <ClotheCard key={ index } clothe={ exampleProduct } />
+          ))
+        }
       </div>
     </div>
-  );
-};
+  )
+}
