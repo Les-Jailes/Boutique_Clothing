a80814d500b97d90cb4bcef668f30a272b859b23
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2023-11-08 03:56:13 -0400

    New styles were added to the user profile page.

--- src/app/pages/user-profile/page.jsx
+++ src/app/pages/user-profile/page.jsx
@@ -1,30 +1,52 @@
+
 import React from 'react'
-import { BiSolidUserCircle } from 'react-icons/bi';
-import "@/app/pages/user-profile/UserProfile.css"
+import { BiSolidUser, BiSolidUserCircle } from 'react-icons/bi';
+import { BsLockFill } from 'react-icons/bs';
+import { MdOutlineAlternateEmail } from 'react-icons/md';
+
 
 const profile = () => {
   return (
-    <div>
-       <div className='img'>
-        <BiSolidUserCircle className='img__image-user' size={100} />
-      </div>
-      <div className='label-manager'>
-        <form>
-          <label className='label-manager__label-first-name'> First name:</label>
-          <label className='label-manager__label-name' id='name'> Alex </label> <br></br>
-          <label className='label-manager__label-last-name'> Last name:</label>
-          <label className='label-manager__label-lname'id='lname'> Choquevolqueta </label><br></br>
-          <label className='label-manager__label-password'> Password:</label>
-          <label className='label-manager__lpassword' id='password'> ******** </label>
-        </form>
-      </div>
-      <div className="filter-bar"><br></br>
-        <select id="filterSelect">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-        </select>
-      </div>
+    <div className='profile-container'> 
+      <form action=''>
+        <h1>Login</h1>
+
+        <div>
+        <BiSolidUserCircle className='img__image-user' size={100} /> <br></br>
+        </div>
+         
+        <div className='input-box'>
+          <input type="text" placeholder='Name' required/>
+          <BiSolidUser/>
+        </div>
+
+        <div className='input-box'>
+          <input type="text"
+          placeholder='Lastname' required/>
+          <BiSolidUser/>
+        </div>
+
+        <div className='input-box'>
+          <input type="text"
+          placeholder='Email' required/>
+          <MdOutlineAlternateEmail/>
+        </div>
+
+        <div className='input-box'>
+          <input type="password"
+          placeholder='Password' required/>
+          <BsLockFill/>
+        </div>
+
+        <div>
+        <select id="filterSelect" className='input-box'>
+          <option value="option1">Gender</option>
+          <option value="option2">Male</option>
+          <option value="option3">Female</option>
+          </select>
+        </div>
+
+      </form>
     </div>
   )
 }
