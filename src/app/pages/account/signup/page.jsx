0574731b7de965b59d85e176a2799ddba84a5797
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2023-11-07 23:12:33 -0400

    Length validations in fields

--- src/app/pages/account/signup/page.jsx
+++ src/app/pages/account/signup/page.jsx
@@ -5,19 +5,27 @@ import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import {AiOutlineUser,AiOutlineLock, AiOutlineEye,AiOutlineEyeInvisible, AiOutlineFontSize, AiOutlineMail} from 'react-icons/ai'
 import {PiIdentificationCardLight} from 'react-icons/pi'
+import {BsGenderAmbiguous, BsGenderFemale, BsGenderMale} from 'react-icons/bs'
 import { useRouter } from 'next/navigation'
-import {validateEmail, validatePassword} from '@/utils/formValidations'
+import {validateEmail, validatePassword, validateTextField} from '@/utils/formValidations'
 import api from '@/app/api/api'
 
 const SignUp = () => {
   const [passwordVisible, setPasswordVisible] = useState(false)
   const [validationEmail, setValidationEmail] = useState(false);
+  const [validationName, setValidationName] = useState(false);
   const [valiEmailMessage, setValiEmailMessage] = useState('');
+  const [validationLastName, setValidationLastName] = useState(false);
   const [emailInput, setEmailInput] = useState('');
   const [validationPassword, setValidationPassword] = useState(false);
   const [valiPasswordMessage, setValiPasswordMessage] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
-  const [userNameInput, setUsernameInput] = useState('');
+  const [nameInput, setNameInput] = useState('');
+  const [validNameMessage, setValiNameMessage] = useState('');
+  const [validLastNameMessage, setValiLastNameMessage] = useState('');
+  const [ciInput, setCiInput] = useState('');
+  const [lastNameInput, setLastNameInput] = useState('');
+  const [selectedGender, setSelectedGender] = useState('');
 
   const session = useSession()
   const router = useRouter()
@@ -83,9 +91,41 @@ const SignUp = () => {
   const handlePasswordToggle = () => {
     setPasswordVisible(!passwordVisible);
   };
-  const handleUsernameChange = (e) => {
+  const handleNameChange = (e) => {
     const username = e.target.value;
-    setUsernameInput(username);
+    setNameInput(username);
+    validateNameInput(username);
+  };
+  const handleLastNameChange = (e) => {
+    const lastName = e.target.value;
+    setLastNameInput(lastName);
+    validateLastNameInput(lastName);
+  };
+  const handleCiChange = (e) => {
+    const ci = e.target.value;
+    if(ci >= 0) setCiInput(ci);
+  };
+
+  const handleGenderChange = (e) => {
+    setSelectedGender(e.target.value);
+  };
+  const validateNameInput = (text) => {
+    const [isValid, validationResult] = validateTextField(text);
+    if (isValid) {
+      setValidationName(false);
+    } else {
+      setValidationName(true);
+      setValiNameMessage(validationResult);
+    }
+  };
+  const validateLastNameInput = (lastName) => {
+    const [isValid, validationResult] = validateTextField(lastName);
+    if (isValid) {
+      setValidationLastName(false);
+    } else {
+      setValidationLastName(true);
+      setValiLastNameMessage(validationResult);
+    }
   };
 
   return (
@@ -94,23 +134,23 @@ const SignUp = () => {
         <form className={styles.form} onSubmit={handleSubmit}>
           <h1 className={styles.title}>Sign Up</h1>
           <div className={styles.inputBox}>
-          <AiOutlineFontSize className={styles.icon}/>
-            <input type="text" onChange={handleUsernameChange} value={userNameInput} placeholder='Name' className={styles.input} required/>
-            <p className={styles.validation}>{validationEmail ? valiEmailMessage : ''}</p>
+          <AiOutlineUser className={styles.icon}/>
+            <input type="text" onChange={handleNameChange} value={nameInput} placeholder='Name' className={styles.input} maxLength={16} required/>
+            <p className={styles.validation}>{validationName ? validNameMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
           <AiOutlineFontSize className={styles.icon}/>
-            <input type="text" onChange={handleUsernameChange} value={userNameInput} placeholder='Last Name' className={styles.input} required/>
-            <p className={styles.validation}>{validationEmail ? valiEmailMessage : ''}</p>
+            <input type="text" onChange={handleLastNameChange} value={lastNameInput} placeholder='Last Name' className={styles.input} maxLength={16} required/>
+            <p className={styles.validation}>{validationLastName ? validLastNameMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
           <AiOutlineMail className={styles.icon}/>
-            <input type="email" onChange={handleEmailChange} value={emailInput} placeholder='Email' className={styles.input} required/>
+            <input type="email" onChange={handleEmailChange} value={emailInput} maxLength={30} placeholder='Email' className={styles.input} required/>
             <p className={styles.validation}>{validationEmail ? valiEmailMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
           <PiIdentificationCardLight className={styles.icon}/>
-            <input type="number" onChange={handleUsernameChange} value={userNameInput} placeholder='CI' className={styles.input} required/>
+            <input type="number" onChange={handleCiChange} value={ciInput} placeholder='CI' className={styles.input} required/>
             <p className={styles.validation}>{validationEmail ? valiEmailMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
@@ -121,6 +161,7 @@ const SignUp = () => {
               className={styles.input}
               value={passwordInput}
               onChange={handlePasswordChange}
+              maxLength={16}
               required
             />
             <p className={styles.validation}>
@@ -135,6 +176,39 @@ const SignUp = () => {
               <AiOutlineEye className={styles.iconLock} onClick={handlePasswordToggle} />
             )}
           </div>
+          <div className={styles.genderBox}>
+        <label className={styles.label}>Gender:</label>
+        <div className={styles.radioGroup} required>
+          <label>
+            <input
+              type="radio"
+              value="male"
+              checked={selectedGender === "male"}
+              onChange={handleGenderChange}
+
+            />
+            Male
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="female"
+              checked={selectedGender === "female"}
+              onChange={handleGenderChange}
+            />
+            Female
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="ratherNotSay"
+              checked={selectedGender === "ratherNotSay"}
+              onChange={handleGenderChange}
+            />
+            Rather not say
+          </label>
+        </div>
+      </div>
           <button className={styles.button}>Create account</button>
         </form>
         <div className={styles.bottomSignUp}>
