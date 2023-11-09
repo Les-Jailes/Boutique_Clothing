52fd6a06452e57886e03757226b8b19226861d7a
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-11-09 18:05:22 -0400

    Changing img for React-icon for optimization

--- src/components/GoogleAuthentication/GoogleAuthButton.jsx
+++ src/components/GoogleAuthentication/GoogleAuthButton.jsx
@@ -1,21 +1,19 @@
-'use client';
+"use client";
 import React from "react";
-import '../../css/GoogleAuthenticationStyles/GoogleButtonStyles.css';
+import "../../css/GoogleAuthenticationStyles/GoogleButtonStyles.css";
 import { signIn } from "next-auth/react";
-
-
-const iconGoogle = "https://i.postimg.cc/2yChFw3L/google-icon.png";
+import { FcGoogle } from "react-icons/fc";
 
 function GoogleAuthButton() {
   const handleSignIn = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     signIn("google", { callbackUrl: "/" });
   };
 
   return (
-    <button className="google-button-container" onClick={handleSignIn} >
+    <button className="google-button-container" onClick={handleSignIn}>
       <div className="google-icon-wrapper">
-        <img className="google-icon" src={iconGoogle} alt="Google Sign-In" />
+        <FcGoogle className="google-icon" />
       </div>
       <p className="button-text">Google</p>
     </button>
