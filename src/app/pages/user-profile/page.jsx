fbff3eb1b7e24a1243139e32865f8a305c9beaa5
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2023-11-09 09:48:12 -0400

    Added setting of values

--- src/app/pages/user-profile/page.jsx
+++ src/app/pages/user-profile/page.jsx
@@ -1,9 +1,11 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react';
 import { BiSolidUser, BiSolidUserCircle } from 'react-icons/bi';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import '@/app/pages/user-profile/UserProfile.css'
+
 
 const Profile = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -12,30 +14,27 @@ const Profile = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [gender, setGender] = useState('default');
+  const session = useSession()
+  console.log(session)
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
   const handleInputChange = (e) => {
     const { id, value } = e.target;
-    switch (id) {
-      case 'name':
-        setName(value);
-        break;
-      case 'lastName':
-        setLastName(value);
-        break;
-      case 'email':
-        setEmail(value);
-        break;
-      case 'password':
-        setPassword(value);
-        break;
-      default:
-        break;
+    setName(value);
+    setLastName(value);
+    setEmail(value);
+    setPassword(value);
+  }
+  useEffect(() => {
+    const status = session.status;
+
+    if (session && status === 'authenticated') {
+      setName(session.data.user.name);
+      setEmail(session.data.user.email);
     }
-  };
+  }, [session]);
 
   const handleSelectChange = (e) => {
     setGender(e.target.value);
@@ -48,14 +47,14 @@ const Profile = () => {
   ];
 
   return (
-    <div className='profile-container'> 
+    <div className='profile-container'>
       <form action=''>
-        <h1 className='title'>Profile</h1>
+        <h1>Profile</h1>
 
         <div>
           <BiSolidUserCircle className='image-user' size={200} />
         </div>
-        
+
         <div className='input-box'>
           <input
             type="text"
@@ -67,7 +66,7 @@ const Profile = () => {
             className='input-field'
             disabled
           />
-          <BiSolidUser className='icon'/>
+          <BiSolidUser className='icon' />
         </div>
 
         <div className='input-box'>
@@ -81,7 +80,7 @@ const Profile = () => {
             className='input-field'
             disabled
           />
-          <BiSolidUser className='icon'/>
+          <BiSolidUser className='icon' />
         </div>
 
         <div className='input-box'>
@@ -95,7 +94,7 @@ const Profile = () => {
             className='input-field'
             disabled
           />
-          <MdOutlineAlternateEmail className='icon'/>
+          <MdOutlineAlternateEmail className='icon' />
         </div>
 
         <div className='input-box'>
@@ -133,5 +132,4 @@ const Profile = () => {
     </div>
   );
 }
-
 export default Profile;
