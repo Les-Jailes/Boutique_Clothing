44d8e24cd6958322ad952d932e89447d0e9ce05b
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-11-08 20:37:19 -0400

    Adding additionals styles into buttonGoogle

--- src/app/pages/about-us/page.jsx
+++ src/app/pages/about-us/page.jsx
@@ -1,20 +1,17 @@
 "use client";
 import styles from './page.module.css';
 import React from 'react';
-import { signOut } from 'next-auth/react'; // Importar la función signOut de next-auth
+import { signOut } from 'next-auth/react';
 
 const Aboutus = () => {
-  // Función para manejar el cierre de sesión
   const handleSignOut = async () => {
-    // Redirigir al usuario a la página principal después de cerrar sesión
     await signOut({ redirect: false });
-    window.location.href = '/'; // o la ruta a la que quieras redirigir después de cerrar sesión
+    window.location.href = '/pages/account/login'; 
   };
 
   return (
     <div className={styles.container}>
       <div>About us</div>
-      {/* Botón de cierre de sesión */}
       <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
