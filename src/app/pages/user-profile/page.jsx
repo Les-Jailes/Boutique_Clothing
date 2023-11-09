0a8fa46cd5d3d57a2695afcbb515f2e6781425eb
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2023-11-09 03:33:51 -0400

    A new format has been added to the profile module and the functionality to set data in the inputs and selector has been added.

--- src/app/pages/user-profile/page.jsx
+++ src/app/pages/user-profile/page.jsx
@@ -1,54 +1,137 @@
 'use client'
-
-import React from 'react'
+import React, { useState } from 'react'
 import { BiSolidUser, BiSolidUserCircle } from 'react-icons/bi';
-import { BsLockFill } from 'react-icons/bs';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import '@/app/pages/user-profile/UserProfile.css'
 
-const profile = () => {
+const Profile = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [gender, setGender] = useState('default');
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    switch (id) {
+      case 'name':
+        setName(value);
+        break;
+      case 'lastName':
+        setLastName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSelectChange = (e) => {
+    setGender(e.target.value);
+  };
+
+  const genderOptions = [
+    { value: 'default', label: 'Gender' },
+    { value: 'Male', label: 'Male' },
+    { value: 'Female', label: 'Female' },
+  ];
+
   return (
     <div className='profile-container'> 
       <form action=''>
         <h1 className='title'>Profile</h1>
 
         <div>
-        <BiSolidUserCircle className='image-user' size={200} />
+          <BiSolidUserCircle className='image-user' size={200} />
         </div>
-         
+        
         <div className='input-box'>
-          <input type="text" placeholder='Name' required className='input-field'/>
+          <input
+            type="text"
+            placeholder='Name'
+            id='name'
+            value={name}
+            onChange={handleInputChange}
+            required
+            className='input-field'
+            disabled
+          />
           <BiSolidUser className='icon'/>
         </div>
 
         <div className='input-box'>
-          <input type="text" placeholder='Lastname' required className='input-field'/>
+          <input
+            type="text"
+            placeholder='Lastname'
+            id='lastName'
+            value={lastName}
+            onChange={handleInputChange}
+            required
+            className='input-field'
+            disabled
+          />
           <BiSolidUser className='icon'/>
         </div>
 
         <div className='input-box'>
-          <input type="text"
-          placeholder='Email' required className='input-field'/>
+          <input
+            type="text"
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={handleInputChange}
+            required
+            className='input-field'
+            disabled
+          />
           <MdOutlineAlternateEmail className='icon'/>
         </div>
 
         <div className='input-box'>
-          <input type="password"
-          placeholder='Password' required className='input-field'/>
-          <BsLockFill className='icon'/>
-        </div>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Password'
+            id='password'
+            value={password}
+            onChange={handleInputChange}
+            required
+            className='input-field'
+            disabled
+          />
+          {showPassword ? (
+            <AiFillEyeInvisible className='icon' onClick={togglePasswordVisibility} />
+          ) : (
+            <AiFillEye className='icon' onClick={togglePasswordVisibility} />
+          )}
 
-        <div className='input-box'>
-        <select id="filterSelect" className='filter-bar'>
-          <option value="option1" className='options'>Gender</option>
-          <option value="option2" className='options'>Male</option>
-          <option value="option3" className='options'>Female</option>
+          <select
+            id="filterSelect"
+            className='filter-bar'
+            value={gender}
+            onChange={handleSelectChange}
+            disabled
+          >
+            {genderOptions.map((option) => (
+              <option key={option.value} value={option.value} className='options'>
+                {option.label}
+              </option>
+            ))}
           </select>
         </div>
-
       </form>
     </div>
-  )
+  );
 }
 
-export default profile
+export default Profile;
