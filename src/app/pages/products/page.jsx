236bf0aff5489ceb38daacf4d1a374f853e0f96f
saletG08 <salet.gutierrez.13594439@usalesiana.edu.bo>
2023-11-10 12:23:33 -0400

    Add product in localStorage

--- src/app/pages/products/page.jsx
+++ src/app/pages/products/page.jsx
@@ -1,6 +1,5 @@
 "use client";
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import createPagination from "@/utils/Pagination";
 import { ClotheCard } from "@/components/Products/ClotheCard";
@@ -8,37 +7,18 @@ import "@/css/Products/ProductsPage.css";
 import { Pagination } from "@/components/Products/Pagination";
 import data from "@/utils/AddCartProducts.json";
 
-const initialCartData = data;
+export default function Page() {
+  const initialProductData =
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("productData")) || data
+      : data;
 
-const Page = () => {
+  const [productData, setProductData] = useState(initialProductData);
   const [pagination, setPagination] = useState([]);
   const [products, setProducts] = useState([]);
   const [currentlyPagination, setCurrentlyPagination] = useState(0);
   const [leftIsDisable, setLeftIsDisable] = useState(true);
   const [rightIsDisable, setRightIsDisable] = useState(false);
-  const [cart, setCart] = useState(initialCartData.products);
-  const [total, setTotal] = useState(initialCartData.total);
-
-  const updateLocalStorage = (updatedCart) => {
-    const updatedData = {
-      ...initialCartData,
-      products: updatedCart,
-      totalProducts: updatedCart.length,
-      total: updatedCart.reduce((acc, product) => acc + parseFloat(product.price), 0),
-    };
-    localStorage.setItem('cartData', JSON.stringify(updatedData));
-    console.log('Updated Cart Data:', updatedData);
-  };
-
-  useEffect(() => {
-    const data = localStorage.getItem('cartData');
-    if (data) {
-      const parsedData = JSON.parse(data);
-      setCart(parsedData.products);
-      setTotal(parsedData.total);
-      console.log('Retrieved Cart Data:', parsedData);
-    }
-  }, []);
 
   useEffect(() => {
     axios
@@ -55,6 +35,12 @@ const Page = () => {
     setPagination(createPagination(products));
   }, [products]);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("productData", JSON.stringify(productData));
+    }
+  }, [productData]);
+
   const handlePaginationRight = () => {
     let paginationNumber = currentlyPagination + 1;
     if (paginationNumber < pagination.length) {
@@ -66,34 +52,28 @@ const Page = () => {
     }
   };
 
-  useEffect(() => {
-    updateLocalStorage(cart, total);
-  }, [cart, total]);
-
-  useEffect(() => {
-    if (cart.length > 0 || total > 0) {
-      console.log("Shopping cart:", cart);
-      console.log("Cart total:", total);
-      console.log("Selected items:", cart.length);
-    }
-  }, [cart, total]);
-
   const addToCart = (product) => {
-    const existingProduct = cart.find(
+    const existingProduct = productData.products.find(
       (item) => item.code === product.code && item.size === product.size
     );
+
     if (existingProduct) {
-      const updatedCart = cart.map((item) =>
-        item.code === product.code && item.size === product.size
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      );
-      setCart(updatedCart);
-      setTotal((prevTotal) => prevTotal + parseFloat(product.price));
+      setProductData((prevData) => ({
+        ...prevData,
+        products: prevData.products.map((item) =>
+          item.code === product.code && item.size === product.size
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+        total: prevData.total + parseFloat(product.price),
+      }));
     } else {
-      const updatedCart = [...cart, { ...product, quantity: 1 }];
-      setCart(updatedCart);
-      setTotal((prevTotal) => prevTotal + parseFloat(product.price));
+      setProductData((prevData) => ({
+        ...prevData,
+        products: [...prevData.products, { ...product, quantity: 1 }],
+        total: prevData.total + parseFloat(product.price),
+        totalProducts: prevData.totalProducts + 1,
+      }));
     }
   };
 
@@ -124,6 +104,4 @@ const Page = () => {
       />
     </div>
   );
-};
-
-export default Page;
+}
--- src/components/navbar/Cart/CartListSummary.jsx
+++ src/components/navbar/Cart/CartListSummary.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import CartCardSummary from "./CartCardSummary";
-import { exampleProducts } from "@/utils/ExampleSummary.js";
-import '@/css/Cart/CartListSummary.css'
+import "@/css/Cart/CartListSummary.css";
 
 const CartListSummary = ({ isOpen }) => {
+  const [productsAdd, setProducts] = useState([]);
+
+  useEffect(() => {
+    const storedProducts = JSON.parse(localStorage.getItem("productData")) || { products: [] };
+    setProducts(storedProducts);
+  }, []);
+
+  useEffect(() => {
+    const handleStorageChange = (event) => {
+      if (event.key === "productData") {
+        const updatedProducts = JSON.parse(event.newValue) || { products: [] };
+        setProducts(updatedProducts);
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);  
 
   return (
     <div className={`list-cart-summary ${isOpen ? "is-open" : ""}`}>
       <div className="list-products">
-        {exampleProducts.map((product, index) => {
-          return <CartCardSummary product={product} key={index} />;
-        })}
+      {productsAdd.products && productsAdd.products.map((product, index) => {
+        return <CartCardSummary product={product} key={index} />;
+      })}
       </div>
       <a href="/pages/shopping-cart" className="go-cart-page">
         View all products
