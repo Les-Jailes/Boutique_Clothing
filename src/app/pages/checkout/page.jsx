7dbbe170ef722756d8735ed915c02f51b202cfd2
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2023-11-13 17:11:02 -0400

    Page structure

---
+++ src/app/pages/checkout/page.jsx
@@ -0,0 +1,11 @@
+import CheckoutForm from '@/components/Checkout/CheckoutForm'
+import styles from './page.module.css'
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <CheckoutForm />
+  )
+}
+
+export default Checkout
