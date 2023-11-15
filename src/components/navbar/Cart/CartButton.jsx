6da1b0f8312c79aae4c9c0357ffd4987d58fc9b1
saletG08 <salet.gutierrez.13594439@usalesiana.edu.bo>
2023-11-15 00:45:59 -0400

    Visible Pop Up

--- src/components/navbar/Cart/CartButton.jsx
+++ src/components/navbar/Cart/CartButton.jsx
@@ -1,22 +1,35 @@
 "use client";
 
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import CartListSummary from "./CartListSummary";
 import "@/css/Cart/CartButton.css";
+import { CartContext } from "@/components/Products/CartContext";
 
 const CartButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const cartButtonRef = useRef(null);
+  const { cart, updateCart } = useContext(CartContext);
 
   const handleOpen = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleRemoveProduct = (productId) => {
+    updateCart((prevCart) => {
+      const updatedProducts = prevCart.products.filter(
+        (product) => product.id !== productId
+      );
+      return { ...prevCart, products: updatedProducts };
+    });
+  };
+
   useEffect(() => {
     const handleClickOutside = (event) => {
-      const cartData = JSON.parse(localStorage.getItem("cart")) || { products: [] };
-      const cart = cartData.products;
       if (
         cartButtonRef.current &&
         !cartButtonRef.current.contains(event.target) &&
@@ -25,29 +38,28 @@ const CartButton = () => {
         const cartListSummary = document.querySelector(".list-cart-summary");
         if (cartListSummary && cartListSummary.contains(event.target)) {
           return;
-        } else {
-          setIsOpen(false);
+        }
+        if (cart.products.length === 0) {
+          handleClose();
+        }
+        if (cart.products.length > 0) {
+          handleClose();
         }
       }
     };
-
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
-  }, [isOpen]);
+  }, [isOpen, cart.products.length]);
 
   return (
     <>
-      <button
-        ref={cartButtonRef}
-        className="cart-button"
-        onClick={handleOpen}
-      >
+      <button ref={cartButtonRef} className="cart-button" onClick={handleOpen}>
         <AiOutlineShoppingCart color="black" size={24} />
       </button>
       <div onClick={(e) => e.stopPropagation()}>
-        <CartListSummary isOpen={isOpen} />
+        <CartListSummary isOpen={isOpen} onRemoveProduct={handleRemoveProduct} />
       </div>
     </>
   );
