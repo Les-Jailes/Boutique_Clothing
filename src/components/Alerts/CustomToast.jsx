b78c0480bebfed40e1f8663bde19f517d08f84cc
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-11-14 20:37:10 -0400

    Adding Toast Dependecy and Component Toast to show

---
+++ src/components/Alerts/CustomToast.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export const showToast = (message, type = "default") => {
+    const options = {
+      position: "bottom-right",
+      autoClose: 5000, 
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    };
+  
+    switch (type) {
+      case "success":
+        toast.success(message, options);
+        break;
+      case "error":
+        toast.error(message, options);
+        break;
+      case "info":
+        toast.info(message, options);
+        break;
+      case "warning":
+        toast.warn(message, options);
+        break;
+      default:
+        toast(message, options);
+    }
+  }
