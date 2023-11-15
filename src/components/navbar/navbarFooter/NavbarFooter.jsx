f5796e3b1493a3b7022a9737b9e3f7ecbba0a4ef
Carlos Antonio Ballester Panigua <carlos.ballester.10387210@usalesiana.edu.bo>
2023-11-15 12:38:32 -0400

    Added styles to the subcategories

--- src/components/navbar/navbarFooter/NavbarFooter.jsx
+++ src/components/navbar/navbarFooter/NavbarFooter.jsx
@@ -34,29 +34,39 @@ const NavFooter = () => {
     };
   }, [modalIsOpen]);
 
+  const handleSubcategoryClick = (url) => {
+    window.location.href = url; // Redirige a la URL de la subcategoría
+  };
+
   return (
     <div className={style.container}>
       {navFooterItems.map((item, index) => (
         <div key={index} className={style.dropdownContainer}>
           <div
             onClick={() => openModal(index)}
-            className={`${style.link} ${
-              selectedCategory === index ? style.selected : ''
-            }`}
+            className={`${style.link} ${selectedCategory === index ? style.selected : ''}`}
           >
             {item.title}
           </div>
         </div>
       ))}
 
-      {modalIsOpen && (
-        <div
-          ref={modalRef}
-          className={style.modal}
-        >
-          <h2>Categoría seleccionada</h2>
-          <p>¡La categoría {navFooterItems[selectedCategory]?.title} fue seleccionada! Aquí puedes agregar más detalles.</p>
-          <button onClick={closeModal}>Cerrar</button>
+      {modalIsOpen && selectedCategory !== null && (
+        <div ref={modalRef} className={style.modal}>
+          {navFooterItems[selectedCategory]?.subcategories && (
+            <div className={style.horizontalSubcategories}>
+              <ul>
+                {navFooterItems[selectedCategory].subcategories.map((subcategory, index) => (
+                  <li key={index}>
+                    <div className={style.subcategoryItem} onClick={() => handleSubcategoryClick(subcategory.url)}>
+                      {subcategory.img && <img src={subcategory.img} alt={subcategory.alt} />}
+                      <span>{subcategory.name}</span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       )}
     </div>
