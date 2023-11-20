c6c28a4a8edc14a175c35a5069255762001ca158
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2023-11-20 19:07:48 -0400

    hotfix sizes

--- src/utils/SizePopup.jsx
+++ src/utils/SizePopup.jsx
@@ -1,9 +1,11 @@
 import "@/css/Cart/SizePopUp.css";
 
 export const SizePopup = ({ handleSizeSelection, sizes, isSizePopupOpen }) => {
+  const sizeStrings = sizes.map((size) => size.size);
+
   return (
     <div className={`size-popup-container ${isSizePopupOpen ? "is-open" : ""}`}>
-      {sizes.map((size, index) => (
+      {sizeStrings.map((size, index) => (
         <button className="size-popup-container__button" key={index} onClick={() => handleSizeSelection(size)}>
           {size}
         </button>
