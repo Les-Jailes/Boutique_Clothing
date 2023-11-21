6e5e3c5e7888e6cc9f4afcd851d085d787b826e5
saletG08 <salet.gutierrez.13594439@usalesiana.edu.bo>
2023-11-21 00:34:48 -0400

    Style to the table

--- src/app/pages/user-profile/order-history/page.jsx
+++ src/app/pages/user-profile/order-history/page.jsx
@@ -1,50 +1,54 @@
 import React from "react";
-//import "@/css/AddProducts/GetProductForm.css";
 import Image from "next/image";
+import "@/css/OrderHistoryUser/OrderHistoryUserTable.css";
 
 const OrderHistoryUser = () => {
   return (
-    <div className="container">
+    <div className="containerTable">
+      <br />
+      <h1>Hello</h1>
+      <br />
       <table>
         <thead>
           <tr>
-            <th>Code</th>
             <th>Name of Product</th>
+            <th>Status</th>
+            <th>Order Date</th>
             <th>Price</th>
             <th>Category</th>
             <th>Type</th>
             <th>Color</th>
             <th>Size</th>
-            <th>Image</th>
-            <th>Action</th>
           </tr>
         </thead>
 
         <tbody>
           <tr>
-            <td>001</td>
-            <td>Product 1</td>
+            <td>
+              <Image src="" alt="Product" width={180} height={180} priority />
+              Product 1
+            </td>
+            <td>Comprado</td>
+            <td>11/20/2023</td>
             <td>$19.99</td>
             <td>Women</td>
             <td>Shirts</td>
             <td>Blue</td>
             <td>L</td>
-            <td>
-              <Image src="" alt="Product" width={180} height={180} priority />
-            </td>
           </tr>
 
           <tr>
-            <td>002</td>
-            <td>Product 2</td>
+            <td>
+              <Image src="" alt="Product" width={180} height={180} priority />
+              Product 2
+            </td>
+            <td>Comprado</td>
+            <td>11/11/2023</td>
             <td>$21.00</td>
             <td>Men</td>
             <td>Pants</td>
             <td>Black</td>
             <td>M</td>
-            <td>
-              <Image src="" alt="Product" width={180} height={180} priority />
-            </td>
           </tr>
         </tbody>
       </table>
