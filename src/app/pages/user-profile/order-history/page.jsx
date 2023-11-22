998de642b6e3daf0dd141b71f0e443a7b75a467f
saletG08 <salet.gutierrez.13594439@usalesiana.edu.bo>
2023-11-22 17:24:03 -0400

    Adding Products to the Table

--- src/app/pages/user-profile/order-history/page.jsx
+++ src/app/pages/user-profile/order-history/page.jsx
@@ -4,13 +4,11 @@ import React, { useState, useEffect } from "react";
 import api from "@/app/api/api";
 import "@/css/OrderHistoryUser/OrderHistoryUserTable.css";
 import { useSession } from "next-auth/react";
-import { IoMdCloseCircleOutline } from "react-icons/io";
-import { TbEditCircle } from "react-icons/tb";
+import Image from "next/image";
 
 const OrderHistoryUser = () => {
   const session = useSession();
   const [userId, setUserId] = useState(null);
-  const [products, setProducts] = useState([]);
   const [checkoutUserId, setCheckoutUserId] = useState([]);
 
   useEffect(() => {
@@ -18,13 +16,11 @@ const OrderHistoryUser = () => {
       try {
         if (session.status === "authenticated") {
           const user = await api.get('/User/email/' + session.data.user.email);
-          console.log(user.data._id);
           setUserId(user.data._id);
           const checkoutUser = await api.get('/OrderHistory/' + user.data._id);
-          console.log(checkoutUser.data.purchasedProducts[0].code);
           setCheckoutUserId(checkoutUser.data);
         }
-      } catch (error) {   
+      } catch (error) {
         console.error("Error fetching user:", error);
       }
     };
@@ -32,71 +28,49 @@ const OrderHistoryUser = () => {
     getUserId();
   }, [session]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await api.get("/Product");
-        console.log("Response:", response.data);
-        setProducts(response.data);
-      } catch (error) {
-        console.error("Axios Error:", error);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
   return (
     <div className='container'>
-      <div>
-        <button className='add-button'>
-          Add Product
-        </button>
+      <div className='container__your-products'>
+        <h1> Your Products </h1>
       </div>
       <div className='table-container'>
         <table>
           <thead>
             <tr>
-              <th>Code</th>
+              <th>Image</th>
               <th>Name of Product</th>
               <th>Price</th>
               <th>Category</th>
               <th>Type</th>
               <th>Color</th>
-              <th>Sizes</th>
-              <th>Image</th>
-              <th>Action</th>
+              <th>Size</th>
+              <th>Quantity</th>
+              <th>Date</th>
             </tr>
           </thead>
 
           <tbody>
-            {products.map((product) => (
-              <tr key={product._id.$oid}>
-                <td className='code'>{product.code}</td>
-                <td>{product.name}</td>
-                <td className='price'>${product.price}</td>
-                <td className='category'>{product.category}</td>
-                <td className='type'>{product.type}</td>
-                <td className='color'>{product.color?.join(', ')}</td>
-                <td className='size'>
-                  {product.sizes.map((size, index) => (
-                    <div key={index}>
-                      {`${size.size}: ${size.quantity}`}
-                    </div>
-                  ))}
-                </td>
-
-                <td className='image'>
-                  {product.path.length > 0 && (
-                    <img src={product.path[0]} alt={`Product 1`} style={{ width: '50px', height: '50px' }} />
-                  )}
-                </td>
-
-                <td>
-                  <IoMdCloseCircleOutline className='delete-button' />
-                  <TbEditCircle className='edit-button' />
-                </td>
-              </tr>
+            {checkoutUserId.map((order) => (
+              order.purchasedProducts.map((product) => (
+                <tr key={product._id}>
+                  <td className='image'>
+                    {product.path.length > 0 && (
+                      <Image src={product.path[0]} 
+                      alt={`Product`} 
+                      width ={50} 
+                      height= {50} />
+                    )}
+                  </td>
+                  <td>{product.name}</td>
+                  <td className='price'>${product.price}</td>
+                  <td className='category'>{product.category}</td>
+                  <td className='type'>{product.type}</td>
+                  <td className='color'>{product.color?.join(', ')}</td>
+                  <td className='size'>{product.size}</td>
+                  <td>{product.quantity}</td>
+                  <td>{new Date(order.createdAt).toLocaleDateString()}</td>
+                </tr>
+              ))
             ))}
           </tbody>
         </table>
