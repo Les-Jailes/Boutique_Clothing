8aa852909c3ba1bdde4e78d61a9e7be23df00df1
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2023-11-27 21:23:49 -0400

    Limit loader

--- src/components/navbar/User/UserListMenu.jsx
+++ src/components/navbar/User/UserListMenu.jsx
@@ -4,14 +4,6 @@ import UserListOptions from "./UserListOptions";
 
 const UserListMenu = ({ isLogged, isOpen, handleLogOut }) => {
   return (
-<<<<<<< HEAD
-    <div className={ `user-list-container ${ isOpen ? 'is-open' : '' }` }>
-      <SessionOption isLogged={ isLogged } />
-      {isLogged && <UserListOptions />}
-      <button className={ `log-out-option ${ isLogged ? 'is-logged' : '' }` } onClick={ handleLogOut } >
-        Log out
-      </button>
-=======
     <div className={`user-list-container ${isOpen ? 'is-open' : ''}`}>
       {isLogged ? (
         <>
@@ -23,7 +15,6 @@ const UserListMenu = ({ isLogged, isOpen, handleLogOut }) => {
       ) : (
         <SessionOption />
       )}
->>>>>>> 10092d978408b748bc1f8b8880ca4d8221a1e4f3
     </div>
   );
 };
