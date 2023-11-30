ad7d1f110dc3a508d1ce00409b55d2361a6676a8
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2023-11-29 23:09:19 -0400

    fix to bug 18

--- src/app/pages/account/signup/page.jsx
+++ src/app/pages/account/signup/page.jsx
@@ -166,17 +166,17 @@ export default function SignUp() {
           <h1 className={styles.title}>Sign Up</h1>
           <div className={styles.inputBox}>
           <AiOutlineUser className={styles.icon}/>
-            <input type="text" onChange={handleNameChange} value={nameInput} placeholder='Name' className={styles.input} maxLength={16} pattern="[A-Za-z\s]+"  required/>
+            <input type="text" onChange={handleNameChange} value={nameInput} placeholder='Name' className={styles.input} maxLength={100} pattern="[A-Za-z\s]+"  required/>
             <p className={styles.validation}>{validationName ? validNameMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
           <AiOutlineFontSize className={styles.icon}/>
-            <input type="text" onChange={handleLastNameChange} value={lastNameInput} placeholder='Last Name' className={styles.input} maxLength={16} pattern="[A-Za-z\s]+" required/>
+            <input type="text" onChange={handleLastNameChange} value={lastNameInput} placeholder='Last Name' className={styles.input} maxLength={100} pattern="[A-Za-z\s]+" required/>
             <p className={styles.validation}>{validationLastName ? validLastNameMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
           <AiOutlineMail className={styles.icon}/>
-            <input type="email" onChange={handleEmailChange} value={emailInput} maxLength={30} placeholder='Email' className={styles.input} required/>
+            <input type="email" onChange={handleEmailChange} value={emailInput} maxLength={100} placeholder='Email' className={styles.input} required/>
             <p className={styles.validation}>{validationEmail ? valiEmailMessage : ''}</p>
           </div>
           <div className={styles.inputBox}>
