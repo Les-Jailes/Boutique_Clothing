85f1fb5716066ad4036940a14339402acda52623
denisgandel <denis.gandarillas.9355205@usalesiana.edu.bo>
2023-11-30 10:51:59 -0400

    Fix and merge functions

--- src/components/ProductDetail/Product.jsx
+++ src/components/ProductDetail/Product.jsx
@@ -11,6 +11,13 @@ import "@/css/ProductDetail/Product.css";
 import PropTypes from "prop-types";
 import { CartContext } from "../Products/CartContext";
 import Swal from "sweetalert2";
+import { useSession } from "next-auth/react";
+import api from "@/app/api/api";
+import Loader from "@/utils/Loader";
+import {
+  showAlertMessageAutomatically,
+  showErrorMessage,
+} from "@/utils/alerts";
 
 const Product = ({ product }) => {
   const [isLiked, setIsLiked] = useState(false);
@@ -21,6 +28,94 @@ const Product = ({ product }) => {
   const { addToCart } = useContext(CartContext);
 
   const [size, setSize] = useState("");
+
+  const session = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      let wishlist = [];
+      if (localStorage.getItem("wishlist")) {
+        wishlist = JSON.parse(localStorage.getItem("wishlist"));
+      }
+      const isProductInWishlist = wishlist.find((u) => u._id === product._id);
+      setIsLiked(isProductInWishlist);
+      api
+        .get("/User/email/" + session.data.user.email)
+        .then((response) => {
+          const user = response.data;
+          const wishlist = user.wishlist;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [session.status, product]);
+
+  const handleLike = () => {
+    if (session.status === "authenticated") {
+      setIsLogged(true);
+      setIsLoading(true);
+      if (!isLiked) addToWishList();
+      else removeFromWishList();
+    } else {
+      setIsLogged(false);
+      showAlertMessageAutomatically(
+        "You need to be logged in to add products to your wish list."
+      );
+    }
+  };
+
+  const addToWishList = async () => {
+    let wishlist = [];
+    if (localStorage.getItem("wishlist")) {
+      wishlist = JSON.parse(localStorage.getItem("wishlist"));
+    }
+
+    wishlist.push(product);
+
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    try {
+      const user = await api.get("/User/email/" + session.data.user.email);
+      const products = user.data.wishlist;
+      products.push(product);
+      const body = {
+        wishlist: products,
+      };
+      await api.put("/User/" + user.data._id, body);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLiked(true);
+    setIsLoading(false);
+  };
+
+  const removeFromWishList = async () => {
+    let wishlist = JSON.parse(localStorage.getItem("wishlist"));
+
+    wishlist = wishlist.filter((p) => p._id !== clothe._id);
+
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    try {
+      const user = await api.get("/User/email/" + session.data.user.email);
+      const products = user.data.wishlist;
+
+      const updatedWishlist = products.filter(
+        (product) => product._id !== clothe._id
+      );
+
+      const body = {
+        wishlist: updatedWishlist,
+      };
+
+      await api.put("/User/" + user.data._id, body);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLiked(false);
+    setIsLoading(false);
+  };
 
   const handleChangeSize = (newSize) => {
     setSize(newSize);
@@ -58,10 +153,6 @@ const Product = ({ product }) => {
     [product.path.length]
   );
 
-  const handleLike = useCallback(() => {
-    setIsLiked(!isLiked);
-  }, [isLiked]);
-
   const handleLeftPosition = useCallback(() => {
     let leftPosition = currentPosition - 1;
     setLeftButton(leftPosition > 0);
@@ -78,6 +169,7 @@ const Product = ({ product }) => {
 
   return (
     <div className="product-detail-container">
+      {isLoading && <Loader isLoaderVisible={isLoading}/>}
       <div className="left-product-detail-container">
         <ProductCarousel
           product={product}
